3c2974eeb53ff812ef8863d4f9e35feb33bd9ff4
yirgamk <yirgam938@gmail.com>
2024-12-05 15:25:54 +0300

    restaurant_menu.js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,1 +1,3 @@
-
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
