fd86088db8fb641b77a457f5179699b031c97d16
yirgamk <yirgam938@gmail.com>
2024-12-06 14:05:30 +0300

    Update todo_list.js

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -5,3 +5,30 @@ const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 
 let tasks = [];
+
+function addTask() {
+            const taskText = taskInput.value.trim();
+            if (taskText !== "") {
+                tasks.push({ text: taskText});
+                taskInput.value = "";
+                displayTasks();
+            }
+        }
+function displayTasks() {
+            taskList.innerHTML = "";
+            tasks.forEach((task, index) => {
+                const li = document.createElement("li");
+                li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+                    <label for="task-${index}">${task.text}</label>`;
+                li.querySelector("input").addEventListener("change", () => toggleTask(index));
+                taskList.appendChild(li);
+            });
+        }
+
+        function toggleTask(index) {
+            tasks[index].completed = !tasks[index].completed;
+            displayTasks();
+        }
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+ displayTasks();
