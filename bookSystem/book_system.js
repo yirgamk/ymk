197cb1e391723b6f3faa4a245c419f5ffa479680
yirgamk <yirgam938@gmail.com>
2024-12-06 10:28:25 +0300

    Update book_system.js

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -18,3 +18,12 @@ let books = [];
                 alert('Please fill in all fields correctly.');
             }
         }
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+        <p><strong>Book Name: </strong>${book.name}</p>
+        <p><strong>Author Name:</strong> ${book.authorName}</p>
+        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
