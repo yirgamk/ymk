9d9b3ad35fb2524834cfd83d400c4076c20d2bf0
yirgamk <yirgam938@gmail.com>
2024-12-06 14:00:06 +0300

    Create todo_list.js

---
+++ TodoList/todo_list.js
@@ -0,0 +1,7 @@
+const taskInput = document.getElementById("taskInput");
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
