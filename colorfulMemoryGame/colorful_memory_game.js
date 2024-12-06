fbf24239d5f24b7a9ebc1f4538b63ab497c9e18a
yirgamk <yirgam938@gmail.com>
2024-12-06 15:30:52 +0300

    Update colorful_memory_game.js

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -1,1 +1,11 @@
+const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'red', 'blue', 'green', 'purple', 'orange', 'pink'];
+let cards = shuffle(colors.concat(colors));
+let selectedCards = [];
+let score = 0;
+let timeLeft = 30;
+let gameInterval;
 
+const startbtn = document.getElementById('startbtn');
+const gameContainer = document.getElementById('game-container');
+const scoreElement = document.getElementById('score');
+const timerElement = document.getElementById('timer');
