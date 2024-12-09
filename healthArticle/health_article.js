99591af8052a5881ee130b77acb7a7f387927928
yirgamk <yirgam938@gmail.com>
2024-12-09 10:14:40 +0300

    Update health_article.js

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,3 +1,6 @@
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
+ var articleDiv = document.createElement('div');
+  articleDiv.classList.add('article');
+  articleDiv.appendChild(title);
 xhr.send();
